4e75872ef6356341c38185e9f022973ea4668e61
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-10-20 15:12:21 +0800

    missing dependency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "refinement-and-linear")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-math"))
 (define scribblings '(("scribblings/refinement-and-linear.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
