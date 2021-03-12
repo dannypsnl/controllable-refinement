207b6238861f1ca8c2daaaa0781f4010bcba0144
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-03-12 22:40:54 +0800

    try something

--- main.rkt
+++ main.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+(define/contract (sorted? l)
+  (list? . -> . boolean?)
+  (andmap = (sort l <) l))
+
+(define/contract (insert l a)
+  ((and/c list? sorted?) any/c . -> . list?)
+  (append l (list a)))
+
+(insert '(1 2 3) 4)
