3959a5e8d0130335ec65685b6b620e41d0c36bea
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-15 06:34:56 +0800

    update

--- flake.nix
+++ flake.nix
@@ -2,15 +2,21 @@
   description = "controllable-refinement";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils}: flake-utils.lib.eachDefaultSystem (system:
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
     in rec {
-      packages = pkgs;
-      #defaultPackage = pkgs.build;
-      devShell = pkgs.mkShell {
-        #buildInputs = [ pkgs.racket ];
+      defaultPackage = pkgs.stdenv.mkDerivation rec {
+        pname = "controllable-refinement";
+        version = "0";
+        src = ./.;
+        buildInputs = with pkgs; [ racket-minimal ];
+        installPhase = ''
+          raco pkg install --auto
+        '';
       };
+      devShell = pkgs.mkShell {};
     }
   );
 }
